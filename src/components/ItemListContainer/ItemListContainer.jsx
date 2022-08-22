7a4f9a483d2150825208f35a885b4217ff587313
devjitorres <jitorresdeveloper@gmail.com>
2022-08-22 19:19:50 -0300

    Add loader

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,21 +1,28 @@
 import ItemList from "../ItemList/ItemList"
 import { getProducts } from "../../asyncmock"
 import { useEffect, useState} from "react"
+import Loader from "../Loader/Loader"
 
 const ItemListContainer = (props) =>  {
 
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(()  => {
         getProducts().then(response => {
             setProducts(response)
         })
+        .finally(() => setLoading(false))
     }, [])
 
     return(
         <div>
             <h1>{props.greeting}</h1>
-            <ItemList products={products}/>
+            {
+                loading 
+                    ? <Loader />
+                    : <ItemList products={products}/>
+            }   
         </div>
     )
 }
